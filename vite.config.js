71841e8577fedd698ad6ad80c276c1526b876a61
Yaswanth Kumar <yaswanth2193@gmail.com>
2025-07-29 12:46:29 +0530

    Add GitHub Actions deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/reactcicd/',
   plugins: [react()],
-})
+  base: '/reactcicd/', // 👈 Use your repo name here
+});
